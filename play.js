2c9563bcccb3e9fb4def657d5cff3d8ce952b0f1
elodie <elodiebouthors@hotmail.com>
2021-02-10 22:12:50 +0000

    updates

--- play.js
+++ play.js
@@ -1,6 +1,17 @@
 const connect = require('./client');
 console.log('Connecting ...');
+
+
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  const handleUserInput = stdin.on('data', (SIGINT) => {
+    process.exit();
+  });
+  return stdin;
+};
 connect();
-
-
-
+setupInput();
