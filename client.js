8fdcc8d892ae20c9bdde5fd3e66c482052bc1e1f
elodie <elodiebouthors@hotmail.com>
2021-02-10 22:33:30 +0000

    update

--- client.js
+++ client.js
@@ -27,4 +27,4 @@ const connect = function() {
   conn.setEncoding('utf8'); 
   return conn;
 }
-module.exports = connect;
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,17 +1,7 @@
-const connect = require('./client');
+const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 
-
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  const handleUserInput = stdin.on('data', (SIGINT) => {
-    process.exit();
-  });
-  return stdin;
-};
 connect();
 setupInput();
